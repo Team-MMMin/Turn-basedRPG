95c49a088c502b0c50e594a5c4060e373944fa0b
Leeseunggyu3 <firetruck1@naver.com>
2024-07-05 20:34:02 +0900

    💩 Poop: BaseObject 추적
    - BaseObject 기능의 누락으로 인해 작업 중지하여 BaseObject 추적중

--- Assets/@Scripts/Managers/Contents/MapManager.cs
+++ Assets/@Scripts/Managers/Contents/MapManager.cs
@@ -2,14 +2,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
 public class MapManager
 {
-    public void Init()
-    {
-        //MapLoad기능
-        //맵 객체 관리 기능
-        //이동 처리 기능
-        //충돌 데이터 파싱 기능 - 플레이어 또는 객체들이 이동가능한 영역과 불가능한 영역을 식별하는 기능
-        
-    }
+    public GameObject Map { get; private set; }
+    public GameObject MapName { get; private set; }
+    public Grid CellGrid { get; private set; }
+
+//      아래 부분 BaseObject가 무엇이길래 누락되어있는지 확인중
+//    Dictionary<Vector3Int, BaseObject> cells = new Dictionary<Vector3Int, BaseObject>(); 4
+
+    private int MinX;
+    private int MaxX;
+    private int MinY;
+    private int MaxY;
+
+    public Vector3Int World2Cell(Vector3 worldPos) { return CellGrid.WorldToCell(worldPos); }
+    public Vector3 Cell2World(Vector3Int cellPos) { return CellGrid.CellToWorld(cellPos); }
+
+
 }
