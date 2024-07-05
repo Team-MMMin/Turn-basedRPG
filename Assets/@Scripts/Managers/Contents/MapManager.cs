c23fce070fe8057bd8a3843c259aec590a185bcf
Leeseunggyu3 <firetruck1@naver.com>
2024-07-05 12:26:03 +0900

    🚀 Feat: TileData.json에 일반 타일과 불가 타일 추가 정의
    - TileData.json에 일반 타일 및 불가 타일 유형 추가

--- Assets/@Scripts/Managers/Contents/MapManager.cs
+++ Assets/@Scripts/Managers/Contents/MapManager.cs
@@ -6,6 +6,10 @@ public class MapManager
 {
     public void Init()
     {
-
+        //MapLoad기능
+        //맵 객체 관리 기능
+        //이동 처리 기능
+        //충돌 데이터 파싱 기능 - 플레이어 또는 객체들이 이동가능한 영역과 불가능한 영역을 식별하는 기능
+        
     }
 }
