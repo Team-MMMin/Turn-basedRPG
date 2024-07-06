e10d02c63f14d1bc05ed48727918b88446e25aa9
Leeseunggyu3 <firetruck1@naver.com>
2024-07-07 04:32:37 +0900

    🚀 Feat: 변수명 및 자료형 수정
    - 딕셔너리 cells의 이름을 _cells로 변경
    - MapName의 자료형을 string으로 변경

--- Assets/@Scripts/Managers/Contents/MapManager.cs
+++ Assets/@Scripts/Managers/Contents/MapManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using static Define;
@@ -7,18 +8,91 @@ using static Define;
 public class MapManager
 {
     public GameObject Map { get; private set; }
-    public GameObject MapName { get; private set; }
+    public string MapName { get; private set; }
     public Grid CellGrid { get; private set; }
 
-    Dictionary<Vector3Int, BaseController> cells = new Dictionary<Vector3Int, BaseController>();
+    Dictionary<Vector3Int, BaseController> _cells = new Dictionary<Vector3Int, BaseController>();
 
     int MinX;
     int MaxX;
     int MinY;
     int MaxY;
 
-    public Vector3Int World2Cell(Vector3 worldPos) { return CellGrid.WorldToCell(worldPos); }
-    public Vector3 Cell2World(Vector3Int cellPos) { return CellGrid.CellToWorld(cellPos); }
+    public Vector3Int WorldToCell(Vector3 worldPos) { return CellGrid.WorldToCell(worldPos); }
+    public Vector3 CellToWorld(Vector3Int cellPos) { return CellGrid.CellToWorld(cellPos); }
 
     ECellCollisionType[,] _collision;
+
+    public void LoadMap(string mapName)
+    {
+        DestroyMap();
+
+        GameObject map = Managers.Resource.Instantiate(mapName);
+        map.transform.position = Vector3.zero;
+        map.name = $"@Map_{mapName}";
+
+        Map = map;
+        MapName = mapName;
+        CellGrid = map.GetComponent<Grid>();
+
+        ParseCollisionData(map, mapName);
+
+        //SpawnObjectsByData(map, mapName);
+    }
+
+    public void DestroyMap()
+    {
+        ClearObjects();
+
+        if (Map != null)
+            Managers.Resource.Destroy(Map);
+    }
+
+    void ParseCollisionData(GameObject map, string mapName, string tilemap = "Tilemap_Collision")
+    {
+        GameObject collision = Util.FindChild(map, tilemap, true);
+        if (collision != null)
+            collision.SetActive(false); //Collision 불가시화
+
+        // Collision 관련 파일
+        TextAsset txt = Managers.Resource.Load<TextAsset>($"{mapName}Collision");
+        StringReader reader = new StringReader(txt.text);
+
+        MinX = int.Parse(reader.ReadLine());
+        MaxX = int.Parse(reader.ReadLine());
+        MinY = int.Parse(reader.ReadLine());
+        MaxY = int.Parse(reader.ReadLine());
+
+        int xCount = MaxX - MinX + 1;
+        int yCount = MaxY - MinY + 1;
+        _collision = new ECellCollisionType[xCount, yCount];
+
+        for (int y = 0; y < yCount; y++)
+        {
+            string line = reader.ReadLine();
+            for (int x = 0; x < xCount; x++)
+            {
+                switch (line[x])
+                {
+                    case Define.MAP_TOOL_WALL:
+                        _collision[x, y] = ECellCollisionType.Wall;
+                        break;
+                    case Define.MAP_TOOL_NONE:
+                        _collision[x, y] = ECellCollisionType.None;
+                        break;
+                    case Define.MAP_TOOL_SEMI_WALL:
+                        _collision[x, y] = ECellCollisionType.SemiWall;
+                        break;
+                }
+            }
+        }
+    }
+    #region Helpers
+    public void ClearObjects()
+    {
+        _cells.Clear();
+    }
+
+    #endregion
+
 }
