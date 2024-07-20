a54c34c5824f16422856e874bb6bfd56f4257c40
멍멍민 <pr620718@cu.ac.kr>
2024-07-20 16:59:40 +0900

    🚀 Feat : #13 MapManager에서 A* Pathfinding 알고리즘 구현
    - A* Pathfinding 알고리즘을 구현하여 경로 탐색 기능 추가
      - PQNode 구조체 추가: A* 알고리즘에 사용할 우선순위 큐 노드 정의
      - _delta 리스트 추가: 8방향 이동을 위한 델타 값 리스트
      - FindPath 메서드 추가: A* 알고리즘을 사용하여 시작점에서 목표 지점까지의 경로를 찾는 함수 구현
      - CalcCellPathFromParent 메서드 추가: 경로 추적을 위해 부모 노드 정보를 사용하여 경로를 계산하는 함수 구현
    - PriorityQueue 클래스와 함께 사용하여 효율적인 경로 탐색 기능 구현 (698c0b2)

--- Assets/@Scripts/Managers/Contents/MapManager.cs
+++ Assets/@Scripts/Managers/Contents/MapManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using static Define;
@@ -182,6 +184,127 @@ public class MapManager
     public void ClearObjects()
     {
         _cells.Clear();
+    }
+    #endregion
+
+    #region A* PathFinding
+    public struct PQNode : IComparable<PQNode>
+    {
+        public int H;
+        public Vector3Int CellPos;
+        public int Depth;
+
+        public int CompareTo(PQNode other)
+        {
+            if (H == other.H)
+                return 0;
+            return H < other.H ? 1 : -1;
+        }
+    }
+
+    List<Vector3Int> _delta = new List<Vector3Int>()
+    {
+        new Vector3Int(0, 1, 0), // U
+		new Vector3Int(1, 1, 0), // UR
+		new Vector3Int(1, 0, 0), // R
+		new Vector3Int(1, -1, 0), // DR
+		new Vector3Int(0, -1, 0), // D
+		new Vector3Int(-1, -1, 0), // LD
+		new Vector3Int(-1, 0, 0), // L
+		new Vector3Int(-1, 1, 0), // LU
+    };
+
+    public List<Vector3Int> FindPath(BaseController self, Vector3Int startCellPos, Vector3Int destCellPos, int maxDepth = 10)
+    {
+        // 지금까지 제일 좋은 후보 기록
+        Dictionary<Vector3Int, int> best = new Dictionary<Vector3Int, int>();
+        // 경로 추적 용도
+        Dictionary<Vector3Int, Vector3Int> parent = new Dictionary<Vector3Int, Vector3Int>();
+
+        // 현재 발견된 후보 중에서 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
+        PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
+
+        Vector3Int pos = startCellPos;
+        Vector3Int dest = destCellPos;
+
+        // destCellPos에 도착 못하더라도 제일 가까운 애
+        Vector3Int closestCellPos = startCellPos;
+        int closestH = (dest - pos).sqrMagnitude;
+
+        // 시작점 발견 (예약 진행)
+        {
+            int h = (dest - pos).sqrMagnitude;
+            pq.Push(new PQNode() { H = h, CellPos = pos, Depth = 1 });
+            parent[pos] = pos;
+            best[pos] = h;
+        }
+
+        while (pq.Count > 0) 
+        {
+            PQNode node = pq.Pop();
+            pos = node.CellPos;
+
+            if (pos == dest)
+                break;
+
+            if (node.Depth >= maxDepth)
+                break;
+
+            foreach (Vector3Int delta in _delta)
+            {
+                Vector3Int next = pos + delta;
+
+                if (CanGo(next) == false)
+                    continue;
+
+                int h = (dest - next).sqrMagnitude;
+
+                if (best.ContainsKey(next) == false)
+                    best[next] = int.MaxValue;
+
+                if (best[next] <= h)
+                    continue;
+
+                best[next] = h;
+
+                pq.Push(new PQNode() { H = h, CellPos = next, Depth = node.Depth + 1 });
+                parent[next] = pos;
+
+                // 목적지까지는 못 가더라도, 그나마 제일 좋았던 후보 기억
+                if (closestH > h)
+                {
+                    closestH = h;
+                    closestCellPos = next;
+                }
+            }
+        }
+
+        // 제일 가까운 얘라도 찾음
+        if (parent.ContainsKey(dest) == false)
+            return CalcCellPathFromParent(parent, closestCellPos);
+
+        return CalcCellPathFromParent(parent, dest);
+    }
+
+    List<Vector3Int> CalcCellPathFromParent(Dictionary<Vector3Int, Vector3Int> parent, Vector3Int dest)
+    {
+        List<Vector3Int> cells = new List<Vector3Int>();
+
+        if (parent.ContainsKey(dest) == false)
+            return cells;
+
+        Vector3Int now = dest;
+
+        while (parent[now] != now)
+        {
+            cells.Add(now);
+            now = parent[now];
+        }
+
+        cells.Add(now);
+        cells.Reverse();
+
+        return cells;
     }
     #endregion
 }
