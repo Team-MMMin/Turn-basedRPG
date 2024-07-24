0c41210645e607063ad5243092dce96648f5da84
멍멍민 <pr620718@cu.ac.kr>
2024-07-24 10:33:56 +0900

    🚀 Feat : #13 경로 찾기 및 이동 관련 기능 추가
    - 크리처가 이동하는 동안 상태를 업데이트하는 `CoUpdate` 코루틴 추가
      - `ECreatureState`에 따른 상태 업데이트 구현
      - `UpdateMove` 메서드를 통해 이동 상태 업데이트
    - A* 알고리즘을 이용해 경로를 찾고 이동하는 기능 추가(6567e79)
      - `FindPathAndMoveToCellPos` 메서드를 통해 목표 지점으로의 경로를 찾아 이동
      - `CoMoveAlongPath` 코루틴을 통해 경로를 따라 이동 구현
    - `CoLerpToCellPos` 코루틴을 통해 부드럽게 셀 위치로 이동하는 기능 추가

--- Assets/@Scripts/Controller/Creature/CreatureController.cs
+++ Assets/@Scripts/Controller/Creature/CreatureController.cs
@@ -1,4 +1,5 @@
 using Data;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -85,5 +86,115 @@ public abstract class CreatureController : BaseController
 
         // State
         CreatureState = ECreatureState.Idle;
+
+        // Map
+        StartCoroutine(CoLerpToCellPos());
     }
+
+    protected IEnumerator CoUpdate()
+    {
+        while (true)
+        {
+            switch (CreatureState) 
+            {
+                case ECreatureState.Idle:
+                    break;
+                case ECreatureState.Move:
+                    UpdateMove();
+                    break;
+                case ECreatureState.Skill:
+                    break;
+                case ECreatureState.Dead: 
+                    break;
+            }
+
+            yield return null;
+        }
+    }
+
+    protected virtual void UpdateMove() { }
+
+    #region Map
+    public EFindPathResult FindPathAndMoveToCellPos(Vector3 destWorldPos, int maxDepth, bool forceMoveCloser = false)
+    {
+        Vector3Int destCellPos = Managers.Map.WorldToCell(destWorldPos);
+        return FindPathAndMoveToCellPos(destCellPos, maxDepth, forceMoveCloser);
+    }
+
+    public EFindPathResult FindPathAndMoveToCellPos(Vector3Int destCellPos, int maxDepth, bool forceMoveCloser = false)
+    {
+        if (LerpCellPosCompleted == false)
+            return EFindPathResult.Fail_LerpCell;
+
+        // A*
+        List<Vector3Int> path = Managers.Map.FindPath(this, CellPos, destCellPos, maxDepth);
+        if (path.Count < 2)
+            return EFindPathResult.Fail_NoPath;
+
+        EFindPathResult findPathResult = EFindPathResult.None;
+        StartCoroutine(CoMoveAlongPath(path, forceMoveCloser, callback => findPathResult = callback));
+        return findPathResult;
+
+        //int idx = 1;
+        //while (path.Count > idx)
+        //{
+        //    if (forceMoveCloser)
+        //    {
+        //        Vector3Int diff1 = CellPos - destCellPos;
+        //        Vector3Int diff2 = path[idx] - destCellPos;
+        //        if (diff1.sqrMagnitude <= diff2.sqrMagnitude)
+        //            return EFindPathResult.Fail_NoPath;
+        //    }
+
+        //    Vector3Int dirCellPos = path[idx] - CellPos;
+        //    Vector3Int nextPos = CellPos + dirCellPos;
+        //    idx++;
+
+        //    if (Managers.Map.CanGo(nextPos) == false)
+        //        return EFindPathResult.Fail_MoveTo;
+        //}
+
+        //return EFindPathResult.Success;
+    }
+
+    IEnumerator CoMoveAlongPath(List<Vector3Int> path, bool forceMoveCloser, Action<EFindPathResult> callback)
+    {
+        for (int idx = 1; idx < path.Count; idx++)
+        {
+            if (forceMoveCloser)
+            {
+                Vector3Int diff1 = CellPos - path[path.Count - 1];
+                Vector3Int diff2 = path[idx] - path[path.Count - 1];
+                if (diff1.sqrMagnitude <= diff2.sqrMagnitude)
+                {
+                    callback(EFindPathResult.Fail_NoPath);
+                    yield break;
+                }
+            }
+
+            Vector3Int dirCellPos = path[idx] - CellPos;
+            Vector3Int nextPos = CellPos + dirCellPos;
+            //Vector3Int nextPos = path[idx];
+
+            if (Managers.Map.MoveTo(this, nextPos) == false)
+            {
+                callback(EFindPathResult.Fail_MoveTo);
+                yield break;
+            }
+            else
+                yield return new WaitUntil(() => LerpCellPosCompleted);
+        }
+
+        callback(EFindPathResult.Success);
+    }
+
+    protected IEnumerator CoLerpToCellPos()
+    {
+        while (true)
+        {
+            LerpToCellPos(5f);
+            yield return null;
+        }
+    }
+    #endregion
 }
