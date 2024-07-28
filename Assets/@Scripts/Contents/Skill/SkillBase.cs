232bd4091db748fb23d8dda94b8e0d657dd81ad0
멍멍민 <pr620718@cu.ac.kr>
2024-07-28 21:01:40 +0900

    🚀 Feat : #22 SkillBase 클래스 기능 확장
    - Name 속성 추가
    - Level 속성 추가
    - IsSkillUnlocked 속성 추가 (레벨이 0 이상일 때 스킬 사용 가능 여부)
    - DoSkill 메서드 추가 (소유자의 마나가 충분할 때 스킬 사용 가능 여부 확인)
    - UpdateSkillData 메서드 추가 (스킬 레벨에 따른 강화 기능, 스킬 데이터 갱신)
     - TODO: 레벨에 따라 스킬을 강화하는 로직 추가 필요

--- Assets/@Scripts/Contents/Skill/SkillBase.cs
+++ Assets/@Scripts/Contents/Skill/SkillBase.cs
@@ -1,4 +1,4 @@
-using Data;
+﻿using Data;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +9,17 @@ public abstract class SkillBase : InitBase
     public CreatureController Owner { get; private set; }
 
     public Data.SkillData SkillData { get; private set; }
+
+    public string Name { get; protected set; }
+    
+    int level = 0;
+    public int Level
+    {
+        get { return level; }
+        set { level = value; }
+    }
+
+    public bool IsSkillUnlocked { get { return Level > 0; } }
 
     public override bool Init()
     {
@@ -23,4 +34,30 @@ public abstract class SkillBase : InitBase
         Owner = owner;
         SkillData = Managers.Data.SkillDataDic[(int)skillID];
     }
+
+    public virtual bool DoSkill()
+    {
+        if (Owner.Mp >= SkillData.ManaCost)
+            return true;
+
+        return false;
+    }
+
+    public Data.SkillData UpdateSkillData()
+    {
+        int skillID = SkillData.DataID;
+        if (skillID == 0)
+            return SkillData;
+
+        SkillData skillData = new SkillData();
+        // TODO
+        // 레벨에 따라 스킬 강화
+        if (Managers.Data.SkillDataDic.TryGetValue(skillID, out skillData) == false)    // 강화한 스킬 불러오기
+            return SkillData;
+
+        SkillData = skillData;
+        OnChangedSkillData();
+        return SkillData;
+    }
+
 }
