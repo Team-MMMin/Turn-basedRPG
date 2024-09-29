be95754344cda0331b0142ad68f4539d51c73dd7
멍멍민 <pr620718@cu.ac.kr>
2024-09-29 17:20:45 +0900

    🚀 Feat: #36 Magic 02 스킬 추가
    - 마나(MP) 5를 소비하여 적을 공격하는 Magic 02 스킬 추가

---
+++ Assets/@Scripts/Contents/Skill/Mage/Magic02.cs
@@ -0,0 +1,41 @@
+﻿using Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic02 : SkillBase
+{
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        return true;
+    }
+
+    public override void SetInfo(CreatureController owner, Define.ESkillID skillID)
+    {
+        base.SetInfo(owner, skillID);
+
+        Name = "Magic 02";
+    }
+
+    public override bool DoSkill()
+    {
+        if (base.DoSkill() == false) 
+            return false;
+
+        Debug.Log("Magic02");
+
+        foreach (var pos in Size)
+        {
+            CreatureController target = Managers.Map.GetObject(pos) as CreatureController;
+            if (target == null)
+                continue;
+
+            target.OnDamaged((SkillData.DamageMultiplier * Owner.Atk) - target.Def);
+        }
+
+        return true;
+    }
+}
